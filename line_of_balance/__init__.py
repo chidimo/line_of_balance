9ee545eea81e7d5359c263acd512b83814318e36
= <orjichidi95@gmail.com>
2018-02-14 19:39:41 +0400

    put all functions in one file

--- line_of_balance/__init__.py
+++ line_of_balance/__init__.py
@@ -1,7 +1,3 @@
-"""Docstring"""
+from .lob import plot_lob_curve, default_lob, LineOfBalance, illustrate_lob
 
-from .api import plot_curve, default_lob
-from .lob import LineOfBalance
-from .illustrate import make_illus, illustrate
-
-__all__ = ["LineOfBalance", "default_lob", "plot_curve", "illustrate", "make_illus"]
+__all__ = ["LineOfBalance", "default_lob", "plot_lob_curve", "illustrate_lob"]
--- line_of_balance/lob.py
+++ line_of_balance/lob.py
@@ -1,12 +1,320 @@
-"""Line of Balance"""
+"""Line of Balance curve plotter"""
+import re
+import matplotlib.pyplot as plt
+import matplotlib.path as mpath
+import numpy as np
+import openpyxl as OP
+try:
+    import tkinter as tk
+    from tkinter import filedialog
+except:
+    print("""tkinter could not be imported. You should check this error unless you're working on a cloud-based system.
+          When you're done, press ENTER to continue'""")
+    if input():
+        pass
 
-from .utils import excel_table, plot_all_activities
+def open_save(mode="save"):
+    """Return path to open or save file"""
+    root = tk.Tk()
+    root.withdraw()
 
-# pylint: disable-msg=C0103
+    if mode == "open":
+        return filedialog.askopenfilename()
+    elif mode == "save":
+        return filedialog.asksaveasfilename()
+
+def get_int_list(string_list):
+    """Get a list of integers representing various list values"""
+    values = re.split(r'[, \[ \]]', string_list)
+    string_list = []
+    for each in values:
+        try:
+            string_list.append(int(each))
+        except ValueError:
+            continue
+    return string_list
+
+def get_names_list(string_list):
+    """Get list of activity names"""
+    values = re.split(r'[, \[ \]]', string_list)
+    values = [each.strip("'") for each in values if each != '']
+    return values
+
+def parse_data():
+    """Parse project activity data from text file"""
+    try:
+        with open(open_save(mode="open"), "r+") as input_handle:
+            input_file_name = input_handle.readlines()
+    except NameError:
+        print("tk module not found")
+        prompt = input("Please provide a file path\nType 'yes' to use default dataset\nENTER to exit")
+
+        if not prompt:
+            return
+        elif prompt == 'yes':
+            input_file_name = open('input.txt', 'r+')
+        else:
+            input_file_name = open(prompt, 'r+')
+
+        input_file_name = input_file_name.readlines()
+
+        activity_names = input_file_name[0].split('=')[1].strip()
+        man_hours_per_unit = input_file_name[1].split('=')[1].strip()
+        men_per_gang = input_file_name[2].split('=')[1].strip()
+
+        buffer_time = int(input_file_name[3].split('=')[1].strip())
+        productivity_rate = int(input_file_name[4].split('=')[1].strip())
+        number_of_units_to_produce = int(input_file_name[5].split('=')[1].strip())
+        hours_per_day = int(input_file_name[6].split('=')[1].strip())
+        days_per_week = int(input_file_name[7].split('=')[1].strip())
+
+        activity_names = get_names_list(activity_names)
+        man_hours_per_unit = get_int_list(man_hours_per_unit)
+        men_per_gang = get_int_list(men_per_gang)
+
+        return activity_names, man_hours_per_unit, men_per_gang,\
+        buffer_time, productivity_rate, number_of_units_to_produce,\
+        hours_per_day, days_per_week
+    return
+
+def excel_table(headings, cell_values):
+    """Table as excel file"""
+
+    book = OP.Workbook()
+    sheet = book.active
+    sheet.title = "lineOfBalance"
+
+    for idx, item in enumerate(headings):
+        sheet.cell(row=1, column=idx+1, value=item)
+    i = 2
+    for each in cell_values:
+        for idx, item in enumerate(each):
+            sheet.cell(row=i, column=idx+1, value=item)
+        i += 1
+
+    try:
+        name = filedialog.asksaveasfilename() + ".xlsx"
+    except NameError:
+        print("tk not found")
+        name = 'line_ob_balance.xlsx'
+    book.save(name)
+
+def pyplot_table(headings, cell_values):
+    """Draw the line of balance table in a plot"""
+
+    num_rows = len(cell_values[0])
+    num_cols = len(cell_values)
+    cell_width = 2.5
+    cell_height = 0.3
+    fig_width = cell_width * num_cols
+    fig_height = cell_height * (num_rows + 1) # plus 1 for heading
+
+    fig = plt.figure(figsize=(fig_width, fig_height))
+    ax = fig.add_subplot(111)
+    ax.axis('off')
+    lob_table = plt.table(cellText=cell_values,
+                          colLabels=headings,
+                          loc='center',
+                          bbox=[0, 0, 1, 0.5])
+
+    lob_table.auto_set_font_size(False)
+    lob_table.set_fontsize(5)
+    lob_table.scale(2, 2)
+    fig.savefig('line_of_balance_table.png', bbox_inches='tight', bbox_extra_artists=[lob_table])
+    plt.show()
+
+def plot_single_activity(coord1, coord2, coord3, coord4, ymin, ymax):
+    """Generate a single plot from a set of points"""
+
+    _ = plt.figure(figsize=(10, 8), facecolor='lightblue', edgecolor='g')#fig
+    label_x_position = (coord2 + coord3)//2
+    label_y_position = (ymax + ymin)//2
+    space = np.linspace(ymin, ymax, 5)
+    plt.yticks(space)
+    plt.ylabel("Number of units to produce")
+    plt.xlabel("Project duration")
+    plt.tight_layout()
+
+    Path = mpath.Path
+    path_data = [
+        (Path.MOVETO, (coord1, ymin)),
+        (Path.LINETO, (coord3, ymax)),
+        (Path.LINETO, (coord4, ymax)),
+        (Path.LINETO, (coord2, ymin)),
+        (Path.CLOSEPOLY, (coord1, ymin))
+    ]
+    codes, verts = zip(*path_data)
+    path = mpath.Path(verts, codes)
+
+    # plot and fill control points and connecting lines
+    x_vals, y_vals = zip(*path.vertices)
+    plt.plot(x_vals, y_vals, 'k-')
+    plt.fill(x_vals, y_vals, 'g')
+    plt.text(label_x_position, label_y_position, "Plot")
+
+    plt.plot([coord4, coord4], [ymin, ymax], 'r--')
+
+    plt.show()
+
+def plot_all_activities(set_of_points, ymin, ymax):
+    """Generate multiple plot from a collection of points
+
+    Parmaters
+    ---------
+    set_of_points : list
+    """
+
+    project_duration = set_of_points[-1][4] + 1
+
+    fig = plt.figure(facecolor='lightblue', edgecolor='g')
+    axes = plt.gca()
+
+    axes.set_xlim(0, 1.1*project_duration)
+    axes.set_ylim(-0.1, 1.3*ymax)
+    axes.spines['right'].set_visible(False)
+    axes.spines['top'].set_visible(False)
+
+    for each in set_of_points:
+        name = each[0]
+        coord1 = each[1]
+        coord2 = each[2]
+        coord3 = each[3]
+        coord4 = each[4]
+
+        Path = mpath.Path
+        path_data = [
+            (Path.MOVETO, (coord1, ymin)),
+            (Path.LINETO, (coord3, ymax)),
+            (Path.LINETO, (coord4, ymax)),
+            (Path.LINETO, (coord2, ymin)),
+            (Path.CLOSEPOLY, (coord1, ymin))
+        ]
+
+        codes, verts = zip(*path_data)
+        path = mpath.Path(verts, codes)
+        # calculate label position
+        label_x_position = (coord2 + coord3)//2
+        label_y_position = (ymax + ymin)//2
+
+        # plot and fill control points and connecting lines
+        x_vals, y_vals = zip(*path.vertices)
+        plt.plot(x_vals, y_vals, 'b-', label=name)
+        plt.fill(x_vals, y_vals, 'cyan')
+        # add label
+        plt.text(label_x_position, label_y_position, name)
+        # draw dropdown
+        plt.plot([coord4, coord4], [ymin, ymax], 'r--')
+
+    plt.title("Line of Balance Curve")
+
+    plt.annotate('Total project duration is\n{} days'.format(project_duration),
+                 xy=(0, 0), xytext=(project_duration/2, 1.2*ymax),
+                 horizontalalignment='center',
+                 verticalalignment='top')
+
+    space = np.linspace(0, 20, 5)
+    plt.yticks(space)
+    plt.ylabel("Number of units to produce")
+    plt.xlabel("Project duration")
+
+    plt.tight_layout()
+    # plt.legend()
+
+    try:
+        name = open_save()
+    except NameError:
+        print("tk not found")
+        name = 'line_ob_balance'
+
+    plt.savefig(name + ".pdf", facecolor=fig.get_facecolor(), dpi=100)
+    plt.savefig(name + ".png", facecolor=fig.get_facecolor(), dpi=100)
+    plt.show()
+
+def illustrate_lob(coord1, coord2, coord3, coord4, ymin, ymax):
+    """Generate plot from points"""
+
+    fig = plt.figure(figsize=(10, 8), facecolor='lightblue', edgecolor='g')
+
+    label_x_position = (coord2 + coord3)//2
+    label_y_position = (ymax + ymin)//2
+    space = np.linspace(ymin, ymax, 5)
+    plt.yticks(space)
+    plt.ylabel("Number of units to produce")
+    plt.xlabel("Project duration")
+    # plt.tight_layout()
+
+    Path = mpath.Path
+    path_data = [
+        (Path.MOVETO, (coord1, ymin)),
+        (Path.LINETO, (coord3, ymax)),
+        (Path.LINETO, (coord4, ymax)),
+        (Path.LINETO, (coord2, ymin)),
+        (Path.CLOSEPOLY, (coord1, ymin))
+    ]
+    codes, verts = zip(*path_data)
+    path = mpath.Path(verts, codes)
+
+    # plot and fill control points and connecting lines
+    x_vals, y_vals = zip(*path.vertices)
+    plt.plot(x_vals, y_vals, 'b-')
+    plt.fill(x_vals, y_vals, 'cyan')
+    plt.text(label_x_position, label_y_position, "Activity")
+    plt.plot([coord4, coord4], [ymin, ymax], 'r--')
+    plt.title("Line of Balance Illustrator", color='k')
+
+    # add annotations
+    plt.annotate('Start on first section', xy=(coord1, ymin), xytext=(coord1, ymin+5),
+                 arrowprops=dict(arrowstyle='->'), horizontalalignment='center')
+    plt.annotate('End on first section', xy=(coord2, ymin), xytext=(coord2+5, ymin),
+                 arrowprops=dict(arrowstyle='->'), verticalalignment='right')
+    plt.annotate('Start on last section', xy=(coord3, ymax), xytext=(coord3, ymax+4),
+                 arrowprops=dict(arrowstyle='->'), horizontalalignment='center')
+    plt.annotate('End on last section', xy=(coord4, ymax), xytext=(coord4, ymax+2),
+                 arrowprops=dict(arrowstyle='->'), horizontalalignment='center')
+
+    axes = plt.gca()
+    axes.title.set_color('red')
+
+    axes.set_xlim(0, 1.1*coord4)
+    axes.set_ylim(-0.1, 1.3*ymax)
+    axes.spines['right'].set_visible(False)
+    axes.spines['top'].set_visible(False)
+
+    try:
+        name = open_save()
+    except NameError:
+        print("tk not found")
+        name = 'line_ob_balance'
+
+    plt.savefig(name + ".pdf", facecolor=fig.get_facecolor(), dpi=100)
+    plt.savefig(name + ".png", facecolor=fig.get_facecolor(), dpi=100)
+    plt.show()
+
+def default_lob():
+    """Plot curve with default values for 5 activities"""
+    activity_names = ['A', 'B', 'C', 'D', 'E']
+    man_hours_per_unit = [100, 350, 60, 200, 150]
+    men_per_gang = [4, 6, 2, 5, 8]
+    buffer_time = 5
+    productivity_rate = 3
+    number_of_units_to_produce = 20
+    hours_per_day = 8
+    days_per_week = 5
+
+    line_object = LineOfBalance(activity_names,
+                                man_hours_per_unit,
+                                men_per_gang,
+                                buffer_time,
+                                productivity_rate,
+                                number_of_units_to_produce,
+                                hours_per_day,
+                                days_per_week)
+    line_object.generate_curve()
+    line_object.generate_excel_table()
+    return line_object
 
 class LineOfBalance:
     """LINE OF BALANCE
-
     Parameters
     -----------
     activity names : list
@@ -19,16 +327,7 @@ class LineOfBalance:
     days_per_week : int
     ymin : int"""
 
-    def __init__(self,
-                 activity_names,
-                 man_hours_per_unit,
-                 men_per_gang,
-                 buffer_time,
-                 productivity_rate,
-                 number_of_units_to_produce,
-                 hours_per_day,
-                 days_per_week,
-                 ymin=0):
+    def __init__(self, activity_names, man_hours_per_unit, men_per_gang, buffer_time, productivity_rate, number_of_units_to_produce, hours_per_day, days_per_week, ymin=0):
 
         self.activity_names = activity_names
         self.man_hours_per_unit = man_hours_per_unit
@@ -85,7 +384,7 @@ class LineOfBalance:
                 each for each in self.actual_output_rate()]
         return [round(x, 2) for x in sftl]
 
-    def start_end(self):
+    def compute_plot_points(self):
         """Compute four last columns concurrently. First section start and end"""
         start_first = [1]
         end_first = [self.activity_duration_per_unit()[0]]
@@ -113,29 +412,31 @@ class LineOfBalance:
                 self.start_first_to_start_last()[i])
                 end_first.insert(i, start_first[i] + self.activity_duration_per_unit()[i])
 
-        return [round(x, 2) for x in start_first], [round(x, 2) for x in end_first],\
-        [round(x, 2) for x in start_last], [round(x, 2) for x in end_last]
+        return [round(x, 2) for x in start_first],\
+               [round(x, 2) for x in end_first],\
+               [round(x, 2) for x in start_last],\
+               [round(x, 2) for x in end_last]
 
     def start_on_first_section(self):
         """Start on first section for activity"""
-        return self.start_end()[0]
+        return self.compute_plot_points()[0]
 
     def end_on_first_section(self):
         """End on first section for activity"""
-        return self.start_end()[1]
+        return self.compute_plot_points()[1]
 
     def start_on_last_section(self):
         """Start on last section for activity"""
-        return self.start_end()[2]
+        return self.compute_plot_points()[2]
 
     def end_on_last_section(self):
         """End on last section for activity"""
-        return self.start_end()[3]
+        return self.compute_plot_points()[3]
 
     def arrange_values(self):
         """Arrange the values in tabular format"""
-
-        values = [[a, b, c, d, e, f, g, h, i, j, k, l]
+        values = [
+            [a, b, c, d, e, f, g, h, i, j, k, l]
                   for a, b, c, d, e, f, g, h, i, j, k, l in
                   zip(self.activity_names,
                       self.man_hours_per_unit,
@@ -148,7 +449,8 @@ class LineOfBalance:
                       self.start_on_first_section(),
                       self.end_on_first_section(),
                       self.start_on_last_section(),
-                      self.end_on_last_section())]
+                      self.end_on_last_section())
+                ]
         return values
 
     def column_headings(self): # add **kwargs to take additional columns
@@ -167,33 +469,53 @@ class LineOfBalance:
                    "End on last section")
         return columns
 
-    def generate_plot_points(self):
-        """Generate plot points"""
-        return [(a, b, c, d, e) for a, b, c, d, e in
-                zip(self.activity_names,
-                    self.start_on_first_section(),
-                    self.end_on_first_section(),
-                    self.start_on_last_section(),
-                    self.end_on_last_section())]
+    def plot_points_with_labels(self):
+        """Generate points to be used for the plot.
+        Points used in plotting are:
+
+        Start on first section, End on first section,
+        Start on last section, End on last section
+        """
+        return [(a, b, c, d, e) for a, b, c, d, e in zip(self.activity_names,
+                                                         self.start_on_first_section(),
+                                                         self.end_on_first_section(),
+                                                         self.start_on_last_section(),
+                                                         self.end_on_last_section())]
 
     def generate_curve(self):
         """Generate the line of balance curve"""
-        points_to_plot = self.generate_plot_points()
-        plot_all_activities(points_to_plot, self.ymin, self.ymax)
+        plot_all_activities(self.plot_points_with_labels(), self.ymin, self.ymax)
 
     def project_duration(self):
         """Total project duration"""
-        return self.generate_plot_points()[-1][4]+1
+        return self.plot_points_with_labels()[-1][4]+1
 
-    def create_table(self):
+    def generate_excel_table(self):
         """Create table from object"""
         heads = self.column_headings()
         vals = self.arrange_values()
         excel_table(heads, vals)
 
-def main():
-    """Main"""
-    return
+def plot_lob_curve():
+    """Generate curve for user input
+    Returns
+    --------
+    Line of Balance object
+    """
+    data = parse_data()
+
+    line_object = LineOfBalance(data[0],
+                                data[1],
+                                data[2],
+                                data[3],
+                                data[4],
+                                data[5],
+                                data[6],
+                                data[7])
+    line_object.generate_excel_table()
+    line_object.generate_curve()
+    return line_object
 
 if __name__ == "__main__":
-    main()
+    print("Working")
+    illustrate_lob(5, 15, 35, 45, 0, 20)
